4f39e17bf5dcc05ea3ed9f142f79c3a909dde271
Nitin Kumar <nitinkumar@mirafra.com>
2020-12-14 14:18:25 +0530

    Initial Commit

---
+++ ecgtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ecgtest
+//
+//  Created by RAC INFRA RENTAL LLP on 14/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
